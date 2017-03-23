59af8306424a5f9d67501de0fa073d8d8cffdf85
Nicole Watkins <nicole.watkins@detroitlabs.com>
2017-03-23 05:01:38 -0400

    Adding main menu with menu prompts, user messages, and print menu feature.

--- store.js
+++ store.js
@@ -21,7 +21,7 @@ var wipeScreen = function () {
 };
 
 var exit = function() {
-  console.log("You have exited the program.");
+  console.log(userMessages.exit);
   sleep(600);
 };
 
@@ -49,7 +49,7 @@ var descriptionAdd = function(){
 var priceAdd = function (){
 	var itemPrice = sget("Enter price").trim();
 	price= itemPrice;
-	if (priceAdd.length <1 || isFinite(health)===false) {
+	if (price.length <1 || isFinite(price)===false) {
 		console.log("Please enter a valid price.");
 	}
 };
@@ -63,16 +63,77 @@ var addMerchandise = function() {
 		descriptionAdd();
 		priceAdd();
 		// inStockAdd();
+    var newInventory = new inventory(description, price);
+		//include numInStock      
+         totalMerchandise.push(newInventory);
+};
+
+
+
+//***********************************
+
+var removeMerchandise = function() {
+};
+var searchMerchandise = function() {
+
+};
+var modifyMerchandise = function() {
+
+};
+
+//************************************
+ var printMenu = function() {
+    for (var key in mainMenuPrompts) {
+      console.log(mainMenuPrompts[key]);
+    }
+};
+
+
+
+var userMessages = {
+	
+                 exit: "\n You are now logged out",
+                 badInput:"\n Please make a valid entry."
+               };                     
+
+
+
+var mainMenuPrompts = { 
+                        welcome:            "|********************************************************************************|\n" + 
+                                            "|                           Inventory Management                                 |\n" +
+                                            "|********************************************************************************|\n"  
+                                 ,
+                                 
+                        add: "Enter 'Add' or '1' to add inventory to store",
+                        remove: "Enter 'Remove' or '2' to remove inventory",
+                        search: "Enter 'Search' or '3' to search inventory",
+                        modify: "Enter 'Modify' or '4' to view current zoo animals",
+                        exit: "Enter 'Exit' or '5' to exit the Zoo\n"
+                      };
+//MAIN MENU LOGIC
+var mainMenu = function() {
+  wipeScreen();
+  sleep(400);
+  printMenu();
+  sleep(400);
+    var userSelection = sget("What would you like to do?").trim();
     
-
-
-var newInventory = new inventory(description, price);
-//include numInStock      
-            totalMerchandise.push(newInventory);
-           console.log("good");
-           console.log(totalMerchandise)
-
-
+    if(userSelection.toLowerCase() == "add" || userSelection == 1) {
+        addMerchandise();
+    } else if(userSelection.toLowerCase() == "remove" || userSelection == 2) {
+        removeMerchandise();
+    } else if(userSelection.toLowerCase() == "search" || userSelection == 3) {
+        searchMerchandise();
+    } else if(userSelection.toLowerCase() == "modify" || userSelection == 4) {
+        modifyMerchandise();
+    } else if(userSelection.toLowerCase() == "exit" || userSelection == 5) {
+        exit();
+    } else {
+      console.log(userMessages.badInput);
+      mainMenu();
+    }
 }();
+
+
 
 
